395f0e6e37838920bf0872eeb7ab24add6150134
codebyazeredo <matheus.azeredo@hotmail.com>
2024-10-17 12:18:46 -0300

    Feat: add Assets, LayoutGuest e LayoutApp Components

---
+++ resources/views/components/layout-guest.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ env('APP_NAME') }} @isset($pageTitle) - {{ $pageTitle }} @endisset</title>
+    
+    <link rel="icon" href={{ asset('assets/images/favicon.png" type="image/png') }}>
+    <link rel="stylesheet" href={{ asset('assets/bootstrap/bootstrap.min.css') }}>
+    <link rel="stylesheet" href={{ asset('assets/css/main.css') }}> 
+</head>
+
+<body>
+
+    {{ $slot }}
+
+    <script src={{ asset('assets/bootstrap/bootstrap.bundle.min.js') }}></script>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,6 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Mail\Message;
 
-Route::get('/mail', function () {
-  Mail::raw('Teste', function(Message $message){
-      $message->to('teste@teste.com')->subject('teste01')
-      ->from('admin@sysRH.com');
-  });
-  echo 'enviado com sucesso';
-});
+Route::view('/login', 'auth.login');
 
-Route::get('/admin', function(){
-  $admin = User::with('detail', 'department')->find(1);
-  dd($admin->toArray());
-});
