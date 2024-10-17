5aea209c5a3047a620d1198e0528383e776b3e2c
codebyazeredo <matheus.azeredo@hotmail.com>
2024-10-17 12:29:51 -0300

    Feat: login

--- app/View/Components/LayoutApp.php
+++ app/View/Components/LayoutApp.php
@@ -8,17 +8,14 @@ use Illuminate\View\Component;
 
 class LayoutApp extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
+   
+    public $pageTitle;
+
+    public function __construct($pageTitle = null)
     {
-        //
+        $this->pageTitle = $pageTitle;
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
         return view('components.layout-app');
---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-layout-app page-title="Home">
+    <h1 class="text-center">APP INSIDE</h1>
+</x-layout-app>
